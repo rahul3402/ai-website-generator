d2bc5ecb20dd17ff69369be61b21102dd12dfc07
Karan Singhal <karan1149@gmail.com>
2019-06-26 18:36:10 -0400

    Added fake news logic from inspirit program

--- template_website/app.py
+++ template_website/app.py
@@ -1,7 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request, Response, jsonify
+
+from old_reference_master_donotedit_section_4_fakenews import *
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World2!'
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET'])
+def predict():
+  input_text = request.args.get('input')
+  print(input_text) 
+
+  def complete_model_fn(input_text):
+  	url, html = get_data_pair(input_text)
+
+  	curr_X = featurize_data_pair(url, html)
+
+  	model = train_model(combined_train_X, train_y, combined_val_X, val_y)
+
+  	curr_y = model.predict(curr_X)[0]
+
+  	if curr_y < .5:
+  		prediction = 'Real'
+  	else:
+  		prediction = 'Fake'
+
+  	return prediction
+
+
+  prediction = complete_model_fn(input_text)
+  
+  response = {'input': input_text, 'prediction': prediction}
+
+  return jsonify(response)
+
